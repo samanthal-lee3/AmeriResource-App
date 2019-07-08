ab0b7e61dce769eb20b8fd372df4de1f7279a69c
Samantha Lee <samanthal.lee3@gmail.com>
2019-07-08 21:20:07 +0000

    Building resource#create method

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -6,5 +6,15 @@ class ResourcesController < ApplicationController
   def new
     @resource = Resource.new
   end
+
+  def create
+    Resource.create(resource_params)
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:name, :description, :rating)
+  end
   
 end
